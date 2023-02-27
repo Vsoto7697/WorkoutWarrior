780fe9065a249a2fa452501fcfa9475c29740ffd
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2023-02-27 11:00:28 -0500

    set up index.js in routes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,27 @@
 const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
-  console.log(`Server is running on port: ${port}`);
 });
